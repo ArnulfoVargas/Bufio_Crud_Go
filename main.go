213bf93b3827851a0f439be9d36d1534e6c31ca5
Arnulfo Vargas <vargas.mejia.arnulfo04@gmail.com>
2024-01-06 17:21:44 -0600

    Deleting useless comments

--- main.go
+++ main.go
@@ -3,21 +3,5 @@ package main
 import "bufiodb/handlers"
 
 func main() {
-	// handlers.List()
-	// handlers.ListByID(2)
-	// handlers.InsertClient(models.Client{
-	//   Id: 0,
-	//   Name: "Saul",
-	//   Mail: "saul@saul.com",
-	//   Phone: "2468013579",
-	// })
-	// handlers.UpdateClient(models.Client{
-	//   Id: 0,
-	//   Name: "Fernando",
-	//   Mail: "fer@fer.com",
-	//   Phone: "1357924680",
-	// }, 3)
-	// handlers.DeleteClient(3)
-
 	handlers.Run()
 }
